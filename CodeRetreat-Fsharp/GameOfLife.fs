df487e2206b9faa9a4bdec773418541b659c6b20
Romain Berthon <romain.berthon@outlook.com>
2017-11-18 11:57:05 +0100

    Ajout de tests cases

--- CodeRetreat-Fsharp/GameOfLife.fs
+++ CodeRetreat-Fsharp/GameOfLife.fs
@@ -12,12 +12,18 @@ type CellState =
 let nextState nbLivingNeighbours currentCellState =
     Dead
 
-[<Fact>]
-let ``Should die when have less than two alive neighbours``() =
-    test <@ Living |> nextState 1 = Dead @>
+[<Theory>]
+[<InlineData 0>]
+[<InlineData 1>]
+let ``Should die when have less than two alive neighbours`` nbLivingNeighbours =
+    test <@ Living |> nextState nbLivingNeighbours = Dead @>
 
 
-[<Fact>]
-let ``Should die when have more than three alive neighbours``() =
-    test <@ Living |> nextState 4 = Dead @>
-
+[<Theory>]
+[<InlineData 4>]
+[<InlineData 5>]
+[<InlineData 6>]
+[<InlineData 7>]
+[<InlineData 8>]
+let ``Should die when have more than three alive neighbours`` nbLivingNeighbours =
+    test <@ Living |> nextState nbLivingNeighbours = Dead @>
